2cb20f0beb0616d2bf72e45cc0ea05227faa693b
Brandon Li <brandon.li@shrgroupllc.com>
2022-07-27 11:52:23 -0400

    added files

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,55 @@
 pipeline {
-    agent { docker { image 'maven:3.3.3' } }
-      stages {
-        stage('log version info') {
-      steps {
-        sh 'mvn --version'
-        sh 'mvn clean install'
-      }
-    }
-  }
+    agent any
+
+    stages {
+        stage('Pull source code from SCM') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    
+        stage('Build/Compile and Unit Test') {
+            steps {
+                echo 'Build'
+            }
+        }
+        
+        stage('Code Coverage') {
+            steps {
+                echo 'Code Coverage'
+            }
+        }
+        
+        stage('Quality Scan') {
+            steps {
+                echo 'Quality Scan'
+            }
+        }
+        
+        stage('Security Scan') {
+            steps {
+                echo 'Security Scan'
+            }
+        }
+        
+        stage('Push to S3') {
+            steps {
+                echo 'Push'
+            }
+        }
+        
+        stage('Build2') {
+             steps {
+                  echo "Hello World"
+                     echo "Multiline shell steps works too"
+             }
+         }      
+        
+        stage('Upload to AWS') {
+              steps {
+                  s3Upload(file:'HelloWorld.py', bucket:'shred-jenkins-test', path:'jenkins-aws-upload/HelloWorld.py')
+                  }
+              }
+         }
 }
+
--- demo.sh
+++ demo.sh
@@ -1,2 +1,22 @@
 echo "Hello this is Brandon's file"
 echo "I just made some changes 7/26"
+
+#file --mime-type * -F$'\t' | awk -F'\t *' '$2 ~/^text\/plain/ { print $1 }'
+# file --mime-type * -F$'\t' = Determines file type of each file in the current folder (*) and prints two-column list (file name ---- MIME type)
+# awk -F'\t *' '$2 ~/^text\/plain/ { print $1 }' = Parses each line into filename and MIME type
+
+#if [[ $(file -b --mime-type "$file") == 'text/plain'* ]]
+#then
+#    echo "Found log file"
+#else
+#    echo "Not found"
+#fi
+
+# -f return true if file exists and regular file
+
+if [ -f ex1.txt ];
+then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi
